b404ae688f6566d76f5fd8a73a91bb02931a9fc0
lilvedaes <daniela.venturo@utec.edu.pe>
2020-05-15 23:13:09 -0400

    added my own superuser function

---
+++ homePage/management/commands/mysuperuser.py
@@ -0,0 +1,11 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='lilvedaes').exists():
+            User.objects.create_superuser('lilvedaes',
+                                          'daventuro@gmail.com',
+                                          'Peruvian-whovian1')
