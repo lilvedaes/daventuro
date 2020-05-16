25fb14017cda16cd15bea2e9bbd5898ee7c32bc0
lilvedaes <daniela.venturo@utec.edu.pe>
2020-05-16 18:13:28 -0400

    added www.daventuro.com allowed host

--- daventuroApp/settings.py
+++ daventuroApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q1wh&d%ge$=23f0lr21h&#e=wgh+het!td=9tg0wctlv7h#x7o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-6mupveq9.us-west-2.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-env.eba-6mupveq9.us-west-2.elasticbeanstalk.com', '127.0.0.1', 'www.daventuro.com']
 
 
 # Application definition
